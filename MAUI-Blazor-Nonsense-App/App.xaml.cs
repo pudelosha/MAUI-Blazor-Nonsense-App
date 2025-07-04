fd6e46601905845e9de5a9aee3eee08f4e0e0a71
pudelosha <pudel1985@gmail.com>
2025-07-04 22:40:04 +0200

    Initial commit

---
+++ MAUI-Blazor-Nonsense-App/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MAUI_Blazor_Nonsense_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "MAUI-Blazor-Nonsense-App" };
+        }
+    }
+}
---
+++ MAUI-Blazor-Nonsense-App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_Blazor_Nonsense_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
